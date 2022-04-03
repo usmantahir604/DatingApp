b7a3594e316e5759ed4e5789461cc836eafda280
Usman Tahir <Usman.tahir@synergy-it.com>
2022-04-04 01:04:03 +0500

    change in buggy controller

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -23,7 +23,7 @@ namespace API.Controllers
         [HttpGet("not-found")]
         public ActionResult<AppUser> GetNotFound()
         {
-            var thing = _context.Users.Find(-1);
+            var thing = _context.Users.Find("-1");
 
             if (thing == null) return NotFound();
 
