3b8df9ab38703a5cdede80d0e8b4cae27a6b4c33
Usman Tahir <Usman.tahir@synergy-it.com>
2022-05-30 23:14:30 +0500

    Authenticating to SignalR

--- API/DependecyInjection/DependencyInjection.cs
+++ API/DependecyInjection/DependencyInjection.cs
@@ -114,6 +114,21 @@ namespace API.DependecyInjection
                 x.SaveToken = true;
                 x.RequireHttpsMetadata = false;
                 x.TokenValidationParameters = tokenValidationParameters;
+                //Authenticating to SignalR
+                x.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+                        var path = context.HttpContext.Request.Path;
+                        if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs"))
+                        {
+                            context.Token = accessToken;
+                        }
+
+                        return Task.CompletedTask;
+                    }
+                };
             });
 
             services.AddAuthorization(opt =>
