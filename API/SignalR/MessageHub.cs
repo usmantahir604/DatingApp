3b8b9aeacd669731fb5d7e8ac2c9bc13b68fc6ac
Usman Tahir <Usman.tahir@synergy-it.com>
2022-06-03 01:16:12 +0500

    Creating a message hub

---
+++ API/SignalR/MessageHub.cs
@@ -0,0 +1,40 @@
+﻿using API.Extensions;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.SignalR;
+
+namespace API.SignalR
+{
+    public class MessageHub :Hub
+    {
+        private readonly IMessageService _messageService;
+        private readonly IMapper _mapper;
+        public MessageHub(IMessageService messageService, IMapper mapper)
+        {
+            _messageService = messageService;
+            _mapper = mapper;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var httpContext=Context.GetHttpContext();
+            var otherUser=httpContext.Request.Query["user"].ToString();
+            var groupName=GetGroupName(Context.User.GetUsername(),otherUser);
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            var messages = _messageService.GetMessagesThread(Context.User.GetUsername(), otherUser);
+
+            await Clients.Group(groupName).SendAsync("RecieveMessageThread", messages);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        private string GetGroupName(string caller, string other)
+        {
+            var stringCompare=string.CompareOrdinal(caller, other)<0;
+            return stringCompare ? $"{caller}-{other}" : $"{other}-{caller}";
+        }
+    }
+}
