2732fb8f853caf43fdc6e250d2665207185c0f2c
Usman Tahir <Usman.tahir@synergy-it.com>
2022-06-03 23:54:26 +0500

    Adding the send message method to the hub

--- API/SignalR/MessageHub.cs
+++ API/SignalR/MessageHub.cs
@@ -1,4 +1,6 @@
-﻿using API.Extensions;
+﻿using API.DAL.Message.Models;
+using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.SignalR;
@@ -9,10 +11,12 @@ namespace API.SignalR
     {
         private readonly IMessageService _messageService;
         private readonly IMapper _mapper;
-        public MessageHub(IMessageService messageService, IMapper mapper)
+        private readonly IUserService _userService;
+        public MessageHub(IMessageService messageService, IMapper mapper, IUserService userService)
         {
             _messageService = messageService;
             _mapper = mapper;
+            _userService = userService;
         }
 
         public override async Task OnConnectedAsync()
@@ -31,6 +35,31 @@ namespace API.SignalR
             await base.OnDisconnectedAsync(exception);
         }
 
+        public async Task SendMessage(CreateMessageModel createMessageModel)
+        {
+            var username = Context.User.GetUsername();
+            if (username == createMessageModel.RecipientUsername.ToLower())
+               throw new HubException("You cannot send message to yourself");
+            var sender = await _userService.GetUserByUsernameAsync(username);
+            var recipient = await _userService.GetUserByUsernameAsync(createMessageModel.RecipientUsername);
+            if (recipient == null) throw new HubException("Not found user");
+            var message = new Message
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUsername = sender.UserName,
+                RecipientUsername = recipient.UserName,
+                Content = createMessageModel.Content
+            };
+
+            _messageService.AddMessage(message);
+
+            if (await _messageService.SaveAllAsync())
+            {
+                var group=GetGroupName(sender.UserName, recipient.UserName);
+                await Clients.Group(group).SendAsync("NewMessage",_mapper.Map<MessageModel>(message));
+            }
+        }
         private string GetGroupName(string caller, string other)
         {
             var stringCompare=string.CompareOrdinal(caller, other)<0;
